fbec41f9e108ba4792bc4561f077bfe8ae00ecae
Krzysztof Hałubek <98700492+MakuwaDev@users.noreply.github.com>
2022-02-01 14:56:05 +0100

    Update unionFind.cpp

--- miscellaneous/unionFind.cpp
+++ miscellaneous/unionFind.cpp
@@ -1,1 +1,19 @@
+//Functions needed to perform find and union operations on an indexed set of elements
+//author: Krzysztof Hałubek
+#include <iostream>
+#include <algorithm>
 
+constexpr size_t MAXN = 200000; //insert maximum amount of elements
+
+size_t representative[MAXN];
+
+size_t Find(size_t x)
+{
+	if (representative[x] == x) return x;
+	return representative[x] = Find(representative[x]);
+}
+
+void Union(size_t x, size_t y)
+{
+	if (Find(x) != Find(y)) representative[Find(x)] = Find(y);
+}
