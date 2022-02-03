e8e2f4019f73876edf3aa856d7dfd7b1cc67eeb2
Krzysztof Hałubek <98700492+MakuwaDev@users.noreply.github.com>
2022-02-03 15:27:02 +0100

    Create maxRoute.cpp

---
+++ dynamicProgramming/maxRoute.cpp
@@ -0,0 +1,27 @@
+//Algorithm to calculate the maximum sum of values from visited fields on a n x m rectangle going from upper left corner to lower right corner
+//author: Krzysztof Hałubek
+#include <iostream>
+
+constexpr int MAXN = 1e3 + 10;
+constexpr int MAXM = 1e3 + 10;
+
+long long dp[MAXN][MAXN];
+int n, m;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            std::cin >> dp[i][j];
+    for (int i = 2; i <= n; i++)
+        dp[i][1] += dp[i - 1][1];
+    for (int i = 2; i <= m; i++)
+        dp[1][i] += dp[1][i - 1];
+    for (int i = 2; i <= n; i++)
+        for (int j = 2; j <= m; j++)
+            dp[i][j] += std::max(dp[i - 1][j], dp[i][j - 1]);
+    std::cout << dp[n][m];
+}
