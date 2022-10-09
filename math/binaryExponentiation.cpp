2249fbf0f94455fb796230d2e74a132f40e23604
Krzysztof Hałubek <98700492+MakuwaDev@users.noreply.github.com>
2022-10-09 13:42:52 +0200

    Create binaryExponentiation.cpp

---
+++ math/binaryExponentiation.cpp
@@ -0,0 +1,24 @@
+//Program to compute kth power of n in log2(k) using binary exponentiation
+
+#include <iostream>
+static inline long long power_fast(long long a, long long pow)noexcept
+{
+	long long i;
+	long long result = 1;
+	long long x = a;
+	for (i = 1; i <= pow; i <<= 1)
+	{
+		if ((pow & i) != 0) result *= x;
+		x *= x;
+	}
+	return result;
+}
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  long long n, k;
+  std::cin>>n>>k;
+  std::cout<<power_fast(n, k);
+}
